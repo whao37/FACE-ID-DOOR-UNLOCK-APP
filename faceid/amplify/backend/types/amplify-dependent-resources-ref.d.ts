ff208fdbf705a9605b5d80b313bb8ab3cf13424d
whao37 <whao37@gatech.edu>
2024-02-09 01:20:01 -0500

    added aws compatibility
    refer to my slides documentation

---
+++ faceid/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "faceid752c69a8": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
